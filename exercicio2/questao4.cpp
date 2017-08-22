8446501931ae40175bd4f83755404801af1e2fe9
Bianca <bianca.rc@hotmail.com>
2017-08-22 17:45:16 -0300

    add questao 4c

--- exercicio2/questao4.cpp
+++ exercicio2/questao4.cpp
@@ -8,8 +8,18 @@ bool isCrescent (int *v, int n);
 
 int findPeak (int *v, int first, int last);
 
+int bitonicBinarySearch (int *v, int first, int last, int x, int peak); 
+
+int rightBinarySearch (int *v, int first, int last, int x);
+
+int leftBinarySearch (int *v, int first, int last, int x);
+
 int main () {
 	int n;
+	bool bitonic;
+	int peak;
+	int x;
+	int pos;
 
 	cout << "Enter the size of your array: " << endl;
 
@@ -33,9 +43,23 @@ int main () {
 	}
 	cout << "]" << endl;
 
-	if (isBitonic(v, n - 1)) {
+	bitonic = isBitonic (v, n - 1);
+
+	if (bitonic) {
 		cout << "Your array is bitonic! :)" << endl;
-		cout << "Its peak is in the position " << findPeak (v, 0, n - 1) << "." << endl;
+		peak = findPeak (v, 0, n - 1);
+		cout << "Its peak is in the position " << peak << "." << endl;
+
+		cout << "Enter a number you want to search in your array: " << endl;
+		cin >> x;
+		
+		pos = bitonicBinarySearch (v, 0, n - 1, x, peak);
+
+		if (pos >= 0) {
+			cout << x << " is in the position " << pos << " of your array." << endl;
+		} else {
+			cout << x << " is not an element of your array." << endl;
+		}
 	} else {
 		cout << "Your array is *not* bitonic :(" << endl;
 	}
@@ -78,3 +102,50 @@ int findPeak (int *v, int first, int last) {
 		return findPeak (v, k + 1, last);
 	}
 }
+
+int bitonicBinarySearch (int *v, int first, int last, int x, int peak) {
+	if (first > last) {
+		return -1;
+	} else if (x == v[peak]) {
+		return peak;
+	} else {
+		int search = leftBinarySearch (v, first, peak - 1, x);
+		if (search >= 0) {
+			return search;
+		} else {
+			return rightBinarySearch (v, peak + 1, last, x);
+		}
+	}
+}
+
+int leftBinarySearch (int *v, int first, int last, int x) {
+	if (first > last) {
+		return -1;
+	} else {
+		int k = (first + last) / 2;
+
+		if (v[k] == x) {
+			return k;
+		} else if (v[k] > x) {
+			return leftBinarySearch (v, first, k - 1, x);
+		} else {
+			return leftBinarySearch (v, k + 1, last, x);
+		}
+	}
+}
+
+int rightBinarySearch (int *v, int first, int last, int x) {
+	if (first > last) {
+		return -1;
+	} else {
+		int k = (first + last) / 2;
+
+		if (v[k] == x) {
+			return k;
+		} else if (v[k] < x) {
+			return rightBinarySearch (v, first, k - 1, x);
+		} else {
+			return rightBinarySearch (v, k + 1, last, x);
+		}
+	}
+} 

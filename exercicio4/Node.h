e4b18e7702f68797992d6afc4a7d238856d81934
Bianca <bianca.rc@hotmail.com>
2017-10-15 02:12:37 -0300

    divide program in multiple files

---
+++ exercicio4/Node.h
@@ -0,0 +1,53 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <class T>
+class Node {
+private:
+	T content;
+	Node *next;
+public:
+	Node();
+	Node(T value);
+	Node(T value, Node* n);
+
+	Node* getNext();
+	void setNext(Node* n); 
+
+	T getValue();
+	void setValue(T value);
+};
+
+template <class T>
+Node<T>::Node(T value) {
+	content = value;
+	next = nullptr;
+}
+
+template <class T>
+Node<T>::Node(T value, Node* n) {
+	content = value;
+	next = n;
+}
+
+template <class T>
+Node<T>* Node<T>::getNext() {
+	return next;
+}
+
+template <class T>
+void Node<T>::setNext(Node* n) {
+	next = n;
+}
+
+template <class T>
+T Node<T>::getValue() {
+	return content;
+}
+
+template <class T>
+void Node<T>::setValue(T value) {
+	content = value;
+}
+
+#endif
---
+++ exercicio4/Sequence.h
@@ -0,0 +1,187 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H	
+
+template <class T>
+class Sequence {
+private:
+	Node<T> *first;
+	int quantity;
+public:
+	Sequence();
+
+	int search(T element);
+
+	Node<T>* getFirst();
+	int getQuantity();
+
+	void insertEnd (T element);
+	void insertBegin (T element);
+	void insert (int index, T element);
+
+	T removeEnd ();
+	T removeBegin ();
+	T remove (int index);
+};
+
+template <class T>
+Node<T>::Node() {
+	content = {};
+	next = nullptr;
+}
+
+
+template <class T>
+Sequence<T>::Sequence() {
+	first = new Node<T>();
+	first->setNext(nullptr);
+	quantity = 0;
+}
+
+template <class T>
+int Sequence<T>::search (T element) {
+	if (quantity == 0) {
+		return -1;
+	}
+
+	Node <T> *p = first;
+
+	if (p->getValue() == element) {
+		return 1;
+	}
+
+	for (int i = 1; i < quantity; i++) {
+		p = p->getNext();
+
+		if (p->getValue() == element) {
+			return i + 1;
+		}
+	}
+
+	return -1;
+}
+
+template <class T>
+void Sequence<T>::insertEnd (T element) {
+	if (quantity == 0) {
+		return insertBegin(element);
+	}
+
+	Node <T> *newNode = new Node <T> (element);
+	Node <T> *p = first;
+
+	for (int i = 1; i < quantity; i++) {
+		p = p->getNext();
+	}
+
+	p->setNext(newNode);
+	newNode->setNext(nullptr);
+
+	quantity++;
+}
+
+template <class T>
+void Sequence<T>::insertBegin (T element) {
+	Node <T> *newNode = new Node <T> (element);
+
+	newNode->setNext(first);
+	first = newNode;
+
+	quantity++;
+}
+
+template <class T>
+void Sequence<T>::insert (int index, T element) {
+	if (index <= 1) {
+		return insertBegin (element);
+	} else if (index == quantity + 1) {
+		return insertEnd (element);
+	}
+
+	Node <T> *newNode = new Node <T> (element);
+	Node <T> *p = first;
+
+	for (int i = 1; i < index - 1; i++) {
+		p = p->getNext();
+	}
+
+	newNode->setNext(p->getNext());
+	p->setNext(newNode);
+
+	quantity++;
+}
+
+template <class T>
+T Sequence<T>::removeEnd() {
+	if (quantity <= 1) {
+		return removeBegin();
+	}
+
+	Node <T> *p = first;
+
+	for (int i = 1; i < quantity - 1; i++) {
+		p = p->getNext();
+	}
+
+	T value = p->getValue();
+
+	delete p->getNext();
+	p->setNext(nullptr);
+	
+	quantity--;
+	return value;
+}
+
+template <class T>
+T Sequence<T>::removeBegin() {
+	if (quantity == 0) {
+		return {};
+	}
+
+	Node <T> *p = first;
+	T value = p->getValue();
+	first = first->getNext();
+
+	delete p;
+
+	quantity--;
+	return value;
+}
+
+template <class T>
+T Sequence<T>::remove(int index) {
+	if (index > quantity) {
+		return {};
+	} else if (index == quantity) {
+		return removeEnd();
+	} else if (index == 1) {
+		return removeBegin();
+	}
+
+	Node <T> *p = first;
+
+	for (int i = 1; i < index - 2; i++) {
+		p = p->getNext();
+	}
+
+	Node <T> *q = p->getNext();
+	T value = q->getValue();
+
+	p->setNext(q->getNext());
+	
+	delete q;
+	
+	quantity--;
+	return value;
+}
+
+template <class T>
+Node<T>* Sequence<T>::getFirst() {
+	return first;
+}
+
+template <class T>
+int Sequence<T>::getQuantity() {
+	return quantity;
+}
+
+#endif
